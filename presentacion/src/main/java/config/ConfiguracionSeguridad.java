539f10847b9684bf098a935fa91b91a13035f4b1
fray osorio <frayosorio@gmail.com>
2025-05-17 20:37:59 -0500

    Se ajusta filtro de seguridad para aceptar swagger

--- presentacion/src/main/java/config/ConfiguracionSeguridad.java
+++ presentacion/src/main/java/config/ConfiguracionSeguridad.java
@@ -12,8 +12,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.config.Customizer;
 
 import monedas.api.aplicacion.seguridad.*;
-import seguridad.FiltroSeguridad;
-import seguridad.UsuarioDetalleServicio;
 
 @Configuration
 @EnableWebSecurity
@@ -32,20 +30,23 @@ public class ConfiguracionSeguridad {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable())
-                .cors(Customizer.withDefaults())
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/api/usuarios/validar/**").permitAll()
-                        .requestMatchers(
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**",
-                                "/swagger-ui.html",
-                                "/swagger-resources/**",
-                                "/webjars/**")
-                        .permitAll()
-                        .anyRequest().authenticated())
+                .csrf(csrf -> csrf.disable()) // Deshabilita la protección CSRF
+                .cors(Customizer.withDefaults()) // Activa CORS
+                .authorizeHttpRequests(
+                        (authz) -> authz
+                                .requestMatchers("/api/usuarios/validar/**").permitAll()
+                                .requestMatchers(
+                                        "/swagger-ui/**",
+                                        "/v3/api-docs/**",
+                                        "/swagger-ui.html",
+                                        "/swagger-resources/**",
+                                        "/webjars/**")
+                                .permitAll()
+                                // .requestMatchers("/api/monedas/**").permitAll()
+                                .anyRequest().authenticated())
                 .addFilterAfter(filtro, UsernamePasswordAuthenticationFilter.class)
                 .build();
+
     }
 
 }
